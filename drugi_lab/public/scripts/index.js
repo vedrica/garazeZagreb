3d3e9777ffbacac722713dd72171969e2ba3d95b
Vedran Vrabec <vv54628@fer.hr>
2024-11-10 15:57:18 +0100

    Dodao web stranicu

---
+++ drugi_lab/public/scripts/index.js
@@ -0,0 +1,5 @@
+// document.getElementById('pretraga').addEventListener('click', function() {
+//     window.location.href = '/search'; 
+//     window.location.reload();
+//     console.log("Usao u pretragu");
+// });
