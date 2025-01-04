6fc2e5095a75971d4e6701e8eb469dac7ed83bc4
Vedran Vrabec <165471557+vedrica@users.noreply.github.com>
2025-01-04 15:42:10 +0100

    Delete drugi_lab directory

--- drugi_lab/public/scripts/index.js
+++
@@ -1,5 +0,0 @@
-// document.getElementById('pretraga').addEventListener('click', function() {
-//     window.location.href = '/search'; 
-//     window.location.reload();
-//     console.log("Usao u pretragu");
-// });
